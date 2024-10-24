7090ef9cc2dbd7b88c598f772652c6c7a7d57d19
nohbdy-exe <darkshadows3003@protonmail.com>
2024-10-24 18:59:43 -0500

    final animations

--- Assets/Game Scripts/General Game Scripts/Player_Movement.cs
+++ Assets/Game Scripts/General Game Scripts/Player_Movement.cs
@@ -15,6 +15,7 @@ public class Player_Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        transform.localScale = new Vector2(3,3);
     }
 
     // Update is called once per frame
@@ -39,6 +40,11 @@ public class Player_Movement : MonoBehaviour
 
     void UpdateAnimation() {
         if (rb.velocity != Vector2.zero) {
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+                this.GetComponent<SpriteRenderer>().flipX = true;
+            } else {
+                this.GetComponent<SpriteRenderer>().flipX = false;
+            }
             animator.SetBool("Walking", true);
             animator.SetFloat("Horizontal", rb.velocity.x);
             animator.SetFloat("Vertical",rb.velocity.y);
