c8e981fa8ccb77facbc7e932f16e9df285eab914
nohbdy-exe <darkshadows3003@protonmail.com>
2024-10-28 17:29:09 -0500

    updating options menu

--- Assets/Game Scripts/Menu Scripts/OptionsMenuScript.cs
+++ Assets/Game Scripts/Menu Scripts/OptionsMenuScript.cs
@@ -8,7 +8,7 @@ public class OptionsMenuScript : MonoBehaviour
     //Returns player to the Main Menu
     public void ReturnToMainFromOptions()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(3);
     }
     
     
